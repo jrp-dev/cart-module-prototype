701f739c05e675c9ec5c2b36a7b6ba2ab0f4705d
Jonald Parong <jonald.parong@serino.com>
2022-10-20 17:32:48 +0800

    Removed duplicated folder from repo

--- example/src/Presentation/components/Button.tsx
+++
@@ -1,7 +0,0 @@
-import {buttonTypes} from '../../types/buttonTypes'
-
-export default function Button(data: buttonTypes) {
-    return (
-        <button onClick={() => data.onClickFunc(data.param)}>{data.title}</button>
-    )
-}
--- example/src/Presentation/components/Text.tsx
+++
@@ -1,7 +0,0 @@
-import {buttonTypes} from '../../types/buttonTypes'
-
-export default function Text(data: {data: string}) {
-    return (
-        <span>{data.data}</span>
-    )
-}
